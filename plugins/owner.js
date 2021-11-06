0b4dc8155c78b95bdace28c299d01a8672d8e775
kalippan-minnuz <93814343+kalippan-minnuz@users.noreply.github.com>
2021-11-06 19:09:26 +0530

    Update owner.js

--- plugins/owner.js
+++ plugins/owner.js
@@ -12,7 +12,7 @@ if (Config.WORKTYPE == 'private') {
         }
 
         if (Config.OWNER == 'default') {
-            await message.client.sendMessage(message.jid,'*Kalippanser by kalippan*' , MessageType.text);
+            await message.client.sendMessage(message.jid,'*Amalser by Amal*' , MessageType.text);
         }
         else {
             await message.client.sendMessage(message.jid,Config.OWNER + '\n\n---------------------', MessageType.text);
@@ -30,7 +30,7 @@ else if (Config.WORKTYPE == 'public') {
         }
 
         if (Config.OWNER == 'default') {
-            await message.client.sendMessage(message.jid,'*Jalippan-ser by Midhun-ser 💌 Kalippanser*' , MessageType.text);
+            await message.client.sendMessage(message.jid,'*Amalser by Farhan_Dqz 💌 Amal*' , MessageType.text);
         }
         else {
             await message.client.sendMessage(message.jid,Config.OWNER + '\n\n--------------------', MessageType.text);
