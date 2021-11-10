852ba76ec063f3626524500e4d43ec1c4dab29d2
kalippan-minnuz <93814343+kalippan-minnuz@users.noreply.github.com>
2021-11-11 04:16:55 +0530

    Update setvar.js

--- plugins/setvar.js
+++ plugins/setvar.js
@@ -10,7 +10,7 @@ Asena.addCommand({pattern: 'setvar', fromMe: false, desc: Lang.WP}, (async (mess
     var r_text = new Array ();
     
     
-    r_text[0] = "https://www.linkpicture.com/q/kalippan-minnuz";
+    r_text[0] = "http://imgur.com/a/uZPETBq.png";
     
     
     var i = Math.floor(1*Math.random())
